9404b2fd91253e417ef82e61530475683b95c640
yutamonb <yutamonb@gmail.com>
2019-02-02 09:25:21 +0900

    GETして200かcheck

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const https = require("https");
+const target = process.env.TARGET;
+const slackUrl = process.env.SLACKURL;
+
 exports.handler = async (event, context, callback) => {
-    console.log('hello world');
-}
+  if (!target) {
+    callback("endpoint undefined");
+    return;
+  }
+
+  https.get(target, res => {
+    console.log(res.statusCode);
+    if (res.statusCode == 200) {
+      callback("statusCode 200");
+      return;
+    }
+  });
+};
